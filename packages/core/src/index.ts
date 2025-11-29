b431a45b887b44d3c7be6618d9c2b714586747f3
whatasoda <shota.hatada@whatasoda.me>
2025-11-29 12:36:32 +0900

    chore: arrange exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,31 +1,5 @@
-/**
- * GraphQL Schema Extract
- *
- * A library for filtering GraphQL schemas based on @expose directive.
- * Supports target-based access control with type reachability analysis.
- *
- * @packageDocumentation
- */
-
-// メイン関数
 export { filterSchema } from "./filter-schema";
 
-// Parser関連の純粋関数
-export {
-  createSchemaAnalysis,
-  debugSchemaAnalysis,
-} from "./analysis/schema-analysis";
-
-// Reachability関連の純粋関数
-export {
-  computeReachability,
-  traverseReachableTypes,
-} from "./reachability/reachability";
-
-// AST Filter関連の純粋関数
-export { filterDefinitionsAST } from "./filter/ast-filter";
-
-// 型定義
 export type {
   SchemaAnalysis,
   TypeLevelExposureInfo,
